1a5b8ee4dbd9b61bd87a7e1d64eb2c89211f3604
aglegaspi <lex83nyc@gmail.com>
2019-10-14 15:51:07 -0400

    first commit

---
+++ iOS_Weather_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Weather_App
+//
+//  Created by Alexander George Legaspi on 10/13/19.
+//  Copyright © 2019 Alexander George Legaspi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
