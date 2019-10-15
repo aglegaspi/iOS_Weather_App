13a03f8a1fa127c90bb3f5eff3eef519b5a1d027
aglegaspi <lex83nyc@gmail.com>
2019-10-15 11:58:49 -0400

    set up views in main view controller, set up tab bar and naviagation controllers

---
+++ iOS_Weather_App/Networking/NetworkHelper.swift
@@ -0,0 +1,10 @@
+//
+//  NetworkHelper.swift
+//  iOS_Weather_App
+//
+//  Created by Alexander George Legaspi on 10/15/19.
+//  Copyright © 2019 Alexander George Legaspi. All rights reserved.
+//
+
+import Foundation
+
